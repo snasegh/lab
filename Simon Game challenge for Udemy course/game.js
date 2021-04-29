45c2567f20078a85934c77015daf2a245076e02f
Stig Nicklas Hansen <snh@snasegh.com>
2021-04-29 22:03:07 +0200

    Wont solve a damn thing..

--- Simon Game challenge for Udemy course/game.js
+++ Simon Game challenge for Udemy course/game.js
@@ -24,13 +24,13 @@ for (let i = 0; i < numberOfButtons; i++) {
     document.querySelectorAll(".btn")[i].addEventListener("click", function () {
     const userChosenColour = buttons[i].id;
     userClickedPattern.push(userChosenColour);
+    console.log("You clicked the button " + userChosenColour);
+    console.log("The current user clicked colors are " + userClickedPattern);
+    console.log("Simon has chosen the colors " + gamePattern);
     playSound(userChosenColour);
     buttonAnimation(userChosenColour);
+    checkAnswer();
     nextSequence();
-    checkAnswer(userClickedPattern[i]);
-
-    console.log(userChosenColour);
-    console.log(userClickedPattern);
 
     });  
 
@@ -38,9 +38,10 @@ for (let i = 0; i < numberOfButtons; i++) {
 
 document.addEventListener("keypress", function() {
     if (gameHasStarted == false) {
-        nextSequence();
+        console.log("The game has now started");
         gameHasStarted = true;
         console.log("Game state = " + gameHasStarted);
+        nextSequence();
     }
  });
 
@@ -55,8 +56,39 @@ function nextSequence() {
     if (userClickedPattern != []) {
         userClickedPattern = [];   
     }
+    console.log("A new sequence has started");
+    console.log("--------------------------");
 }
 
+
+/* 
+
+    I'm really stuck..
+    I don't know how to write the code to check if the gamePattern array
+    matches the userClickedPattern array.. It seems to clear in my head,
+    but I can't for whatever reason convert my thoughts into code....
+    
+    To whoever reads this, if I in a future commit found a solution, I got help..
+    Which is probably not a bad thing (to ask for help), but my demons told me I need 
+    to figure this out myself..
+
+
+*/ 
+
+function checkAnswer() {
+    for (let i = 0; i < gamePattern.length; i++) {
+        const element = gamePattern[i];
+
+        if (userClickedPattern[i] == gamePattern[i]) {
+            console.log("You got it right");
+        } else {
+            console.log("you got it wrong");
+        }
+        
+    }
+}
+
+/* 
 function checkAnswer(currentLevel) {
     console.log("The current level is " + currentLevel);
     if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
@@ -65,6 +97,7 @@ function checkAnswer(currentLevel) {
         console.log("Oopps.. The color you selected did not match the game pattern array");
     }
 }
+*/
 
 
 function buttonAnimation(colour) {
