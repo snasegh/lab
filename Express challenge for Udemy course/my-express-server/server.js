423e209f8ab62abde40ef3bc09d4f0c4001a5778
Stig Nicklas Hansen <snh@snasegh.com>
2021-05-10 07:19:19 +0200

    Makes a BMI calculator

--- Express challenge for Udemy course/my-express-server/server.js
+++ Express challenge for Udemy course/my-express-server/server.js
@@ -1,22 +1,26 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(request, response) {
-    console.log(request);
-    response.send("hello");
+    response.sendFile(__dirname + "/index.html");
 });
 
-app.get("/contact", function(request, response) {
-    console.log(request);
-    response.send("contact me at hei@stighansen.no");
+
+app.post("/", function(request, response) {
+
+    console.log(request.body);
+
+    var weight = Number(request.body.weight);
+    var height = Number(request.body.height);
+    var result = (weight / (height * height) * 10000).toFixed(2);
+
+    response.send("Your BMI is: " + result);
 });
 
-app.get("/about", function(request, response) {
-    console.log(request);
-    response.send("A coding product owner");
-});
-
-app.listen(3000, function() {
-    console.log("Server has started on port " + port);
+app.listen(port, function() {
+    console.log("Server is now running on port " + port);
 });
